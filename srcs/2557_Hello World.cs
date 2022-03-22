21aa03b26937de4ade368ec52de65f21ece72b51
pnk4work <102134724+pnk4work@users.noreply.github.com>
2022-03-22 15:39:29 +0900

    add 2557_Hello World.cs

---
+++ srcs/2557_Hello World.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program
+{
+    class _2557
+    {
+        static void Main(string[] argv) => new Solution();
+    }
+    class Solution
+    {
+        public Solution()
+        {
+            Console.Write("Hello World!");
+        }
+
+
+    }
+}
