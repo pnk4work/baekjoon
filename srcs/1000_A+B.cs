92fe0d689829399c164c55edc2553719d0ebebda
pnk4work <102134724+pnk4work@users.noreply.github.com>
2022-03-22 15:55:42 +0900

    Create 1000_A+B.cs

---
+++ srcs/1000_A+B.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace baekjoon.srcs
+{
+    class _1000
+    {
+        //static void Main(string[] argv) => new Solution();
+        class Solution
+        {
+            public Solution()
+            {
+                int[] input = Array.ConvertAll(Console.ReadLine().Trim().Split(), s => int.Parse(s));
+                int[] vals = (from num in input select num).ToArray();
+
+                Console.WriteLine(vals[0] + vals[1]);
+            }
+        }
+    }
+}
