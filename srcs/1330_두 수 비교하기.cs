c8f10b90e36e04ab5315158bcbd2e34aa4feceea
pnk4work <102134724+pnk4work@users.noreply.github.com>
2022-04-05 14:51:21 +0900

    Create 1330_두 수 비교하기.cs

---
+++ srcs/1330_두 수 비교하기.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace baekjoon.srcs
+{
+    class _1330
+    {
+        //static void Main(string[] argv) => new Solution();
+        class Solution
+        {
+            public Solution()
+            {
+
+                int[] input = Array.ConvertAll(Console.ReadLine().Trim().Split(), s => int.Parse(s));
+
+                int A = input[0];
+                int B = input[1];
+
+                if (A > B) Console.WriteLine(">");
+                else if (A < B) Console.WriteLine("<");
+                else Console.WriteLine("==");
+            }
+        }
+    }
+}
